5eacdacbca5a68958f27c12580951eef125e5c25
Aditya-138 <adityaraghuwanshi13082003@gmail.com>
2025-03-07 03:29:11 +0530

    Displaying the next question

--- src/components/quiz/Play.jsx
+++ src/components/quiz/Play.jsx
@@ -1,5 +1,6 @@
 import React,{Component, Fragment} from 'react'
 import { Helmet } from 'react-helmet';
+import M from 'materialize-css';
 
 import questions from '../../questions.json';
 import isEmpty from '../../utils/is-empty';
@@ -48,6 +49,47 @@ class Play extends Component {
         }
     };
 
+    handleOptionClick = (e) =>{
+        if(e.target.innerHTML.toLowerCase() === this.state.answer.toLowerCase()){
+            this.correctAnswer();
+        }
+        else {
+            this.wrongAnswer();
+        }
+    }
+
+    correctAnswer = () => {
+        M.toast({
+            html : 'Correct Answer',
+            classes : 'toast-valid',
+            displayLength :1500
+        });
+        this.setState(prevState => ({
+            score : prevState.score+1,
+            correctAnswers : prevState.correctAnswer+1,
+            currentQuestionIndex: prevState.currentQuestionIndex +1,
+            numberofAnsweredQuestion: prevState.numberofAnsweredQuestion+1 
+        }),()=>{
+            this.displayQuestions(this.state.questions,this.state.correctAnswers,this.state.nextQuestion,this.state.previousQuestion)
+        })
+    }
+
+    wrongAnswer = () => {
+        navigator.vibrate(1000);
+        M.toast({
+            html : 'Wrong Answer',
+            classes : 'toast-invalid',
+            displayLength :1500
+        });
+        this.setState(prevState => ({
+            wrongAnswers : prevState.wrongAnswers+1,
+            currentQuestionIndex: prevState.currentQuestionIndex +1,
+            numberofAnsweredQuestion: prevState.numberofAnsweredQuestion+1 
+        }),()=>{
+            this.displayQuestions(this.state.questions,this.state.correctAnswers,this.state.nextQuestion,this.state.previousQuestion)
+        });
+    }
+
     render (){
         const { currentQuestion } = this.state;
         return (
@@ -71,12 +113,12 @@ class Play extends Component {
                         </div>
                     <h5>{currentQuestion.question}</h5>
                     <div className="options-container">
-                    <p className="option">{currentQuestion.optionA}</p>
-                    <p className="option">{currentQuestion.optionB}</p>
+                    <p onClick={this.handleOptionClick} className="option">{currentQuestion.optionA}</p>
+                    <p onClick={this.handleOptionClick} className="option">{currentQuestion.optionB}</p>
                     </div>
                     <div className="options-container">
-                    <p className="option">{currentQuestion.optionC}</p>
-                    <p className="option">{currentQuestion.optionD}</p>
+                    <p onClick={this.handleOptionClick} className="option">{currentQuestion.optionC}</p>
+                    <p onClick={this.handleOptionClick} className="option">{currentQuestion.optionD}</p>
                     </div>
 
                     <div className="button-container">
